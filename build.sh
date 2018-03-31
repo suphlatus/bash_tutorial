342a5fbab82d0ab1eb4bca9ecb5f545800934f73
Cevat Bostancıoğlu <bostancioglucevat@gmail.com>
2018-03-31 17:27:45 +0300

    FILTER_NAME variable updated. echo message added.

--- build.sh
+++ build.sh
@@ -2,9 +2,12 @@
 
 set -x
 WEB_PAGE="https://www.kali.org/"
+FILTER_NAME="cevat"
 
 mkdir -p isa
 cd isa
 wget -nc ${WEB_PAGE}
-cat index.html | grep kali
+cat index.html | grep ${FILTER_NAME}
+echo "hhaha"
+
 
